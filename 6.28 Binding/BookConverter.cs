bb2933f256f91a6aa0d9cf6bf6ec33ad9bf54eb4
martin <martin@V-DOXIE-W10>
2016-07-01 08:46:39 +0800

    Add project files.

---
+++ 6.28 Binding/BookConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace _6._28_Binding
+{
+    class BookConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (parameter.Equals("Author"))
+            {
+                return "作者:" + value;
+            }
+            else if (parameter.Equals("Title")) {
+                return "书名:" + value;
+            }
+            return value;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return false;
+        }
+    }
+}
---
+++ 6.28 Binding/BookModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._28_Binding
+{
+    public class BookModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged( String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private String image;
+        public String Image  {
+            get { return image; }
+            set {
+                image = value;
+                if (PropertyChanged != null) {
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private String title;
+        public String Title  {
+            get { return title; }
+            set {
+                title = value;
+                if (PropertyChanged != null)
+                {
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private String author;
+        public String Author {
+            get { return author; }
+            set {
+                author = value;
+                if (PropertyChanged != null)
+                {
+                    NotifyPropertyChanged();
+                }
+            }
+        } 
+    }
+
+    public class BookManager {
+        public static ObservableCollection<BookModel> GetMyBooks()
+        {
+            ObservableCollection<BookModel> Books = new ObservableCollection<BookModel>();
+
+            Books.Add(new BookModel { Image = "/Assets/meinv01.png",Title="苍井空",Author="苍老师"});
+            Books.Add(new BookModel { Image = "/Assets/meinv02.png", Title = "小泽", Author = "泽" });
+            Books.Add(new BookModel { Image = "/Assets/meinv03.jpg", Title = "藤原纪香", Author = "小腾" });
+
+            return Books;
+        }
+    }
+}
